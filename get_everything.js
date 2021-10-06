72a0454633c659706d612e2f7e49a54179e8ece2
Paul Mullins <paulmullins@protonmail.com>
2021-10-05 21:11:05 -0400

    CLI appears to be working, time to find bugs

--- get_everything.js
+++ get_everything.js
@@ -106,6 +106,8 @@ async function create_folder_if_not_exist(create_folder_name){
 
 async function get_team_topics(export_dir, keybase_user, team_name){
     let topic_file_name = `${export_dir}/${keybase_user}/teams/${team_name}/topics.json`
+    // console.log("topic_file_name")
+    // console.log(topic_file_name)
     if (  fs.existsSync(`${export_dir}/${keybase_user}/teams/${team_name}/topics.json`)  ) {
         return JSON.parse(await fs.readFileSync(topic_file_name, 'utf8'))
     }
@@ -138,6 +140,38 @@ async function get_team_topics(export_dir, keybase_user, team_name){
     }
 }
 
+async function export_team_topics(export_dir, keybase_user, export_team_name){
+    console.log(export_team_name)
+    create_folder_if_not_exist(`${export_dir}/${keybase_user}`)
+    create_folder_if_not_exist(`${export_dir}/${keybase_user}/teams`)
+    create_folder_if_not_exist(`${export_dir}/${keybase_user}/teams/${export_team_name}`)
+    let team_topics = await get_team_topics(export_dir, keybase_user, export_team_name)
+    for(var i = 0; i < team_topics.result.conversations.length; i++){
+        let tmp_topic_messages = await get_keybase_topic(
+            team_topics.result.conversations[i].channel.name,
+            team_topics.result.conversations[i].channel.members_type,
+            team_topics.result.conversations[i].channel.topic_name
+        )
+        tmp_topic_messages.forEach((element) => {
+            if (element.msg.content.type == "text"){
+                var urls = extractUrls(element.msg.content.text.body, true);
+                if (urls != undefined){
+                    console.log(urls)
+                    element.msg.urls = urls
+                    element.msg.url_num = urls.length
+                    element.msg.domains = [ ...new Set(extractDomain(urls))]
+                    element.msg.domains_num = element.msg.domains.length
+                }
+            }
+        })
+        fs.writeFileSync(`${export_dir}/${keybase_user}/teams/${export_team_name}/${team_topics.result.conversations[i].channel.topic_name}.json`, JSON.stringify(tmp_topic_messages), (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log(`export of topic ${team_topics.result.conversations[i].channel.topic_name} for team ${export_team_name} is saved in ${tmp_file_output}.`);
+        });
+    }
+}
 
 async function main() {
     let keybase_user;
@@ -201,4 +235,12 @@ async function main() {
 
     process.exit(1)
 }
-main()
+//main()
+
+export {
+    get_keybase_user,
+    export_team_memberships,
+    create_folder_if_not_exist,
+    export_team_topics,
+    get_team_topics
+}
--- keybase-binding-cli.js
+++ keybase-binding-cli.js
@@ -1,5 +1,7 @@
 // index.ts
 import { Command } from 'commander';
+import { get_team_topics, create_folder_if_not_exist, get_keybase_user, export_team_memberships, export_team_topics } from './get_everything.js';
+
 const program = new Command();
 
 program
@@ -10,13 +12,47 @@ program
   .option('-a --attachments <type>', 'Chose weather to export attachments', false)
   .option('-p --path <type>', 'Export Location', './exports');
 
-program.parse(process.argv);
-
-const options = program.opts();
-if (options.debug) console.log(options);
-console.log('pizza details:');
-console.log("options.teams", options.teams)
-console.log("options.team-chats", options.teamchats)
-console.log("options.group-chat", options.groupchat)
-console.log("options.attachments", options.attachments)
-console.log("options.path", (options.path))
+async function main() {
+  program.parse(process.argv);
+  const options = program.opts();
+  let keybase_user = await get_keybase_user();
+  // console.log('CLI Variables:');
+  // console.log("options.teams", options.teams)
+  // console.log("options.teams", options.groups)
+  // console.log("options.team-chats", options.teamchats)
+  // console.log("options.group-chat", options.groupchat)
+  // console.log("options.attachments", options.attachments)
+  // console.log("options.path", options.path)
+  create_folder_if_not_exist(options.path)
+  create_folder_if_not_exist(`${options.path}/${keybase_user}`)
+  create_folder_if_not_exist(`${options.path}/${keybase_user}/teams`)
+  let team_memberships = await export_team_memberships(
+    `${options.path}/${keybase_user}/team_memberships.json`
+  )
+  // Saves in options.path, user / teams / team_name
+  // TODO CREATE DIRECTORIES IF NOT THERE
+  if (options.teamchats){
+    console.log("IT RAN")
+    let team_list = [];
+    team_memberships.forEach((team) => {
+      team_list.push(team.Team)
+    })
+    if (team_list.indexOf(options.teamchats) > 1 ){
+      console.log(`Exporting ${options.teamchats}`)
+      // let team_topics = await get_team_topics(options.path, keybase_user, options.teamchats)
+      // export_team_topics
+      // console.log(team_topics)
+      await export_team_topics(options.path, keybase_user, options.teamchats)
+    } else {
+      console.log(`You do not appear to be on team ${options.teamchats}`)
+    }
+  }
+  
+  // let team_topics = await export_team_topic(
+  //   options.path,
+  //   keybase_user,
+  //   "dentropydaemon"
+  // )
+  process.exit(1)
+}
+main()
